66b6ced22c24670527322943a301ee71143bbe33
王晓勃 <wangxiaobo@icloud.com>
2019-11-03 17:16:38 +0800

    fix(funcs): functions

---
+++ funcs/functions.go
@@ -0,0 +1,14 @@
+package funcs
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+// RequestID 生成request_id
+func RequestID() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return strconv.FormatInt(time.Now().UnixNano(), 10) + fmt.Sprintf(":%d", r.Intn(10000))
+}
