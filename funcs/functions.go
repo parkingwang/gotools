b04d9da16f626a6f11875336c840cd6b33bbea7a
王晓勃 <wxb0328@icloud.com>
2019-12-24 16:39:12 +0800

    feat(funcs): 新增GID()获取 goroutine ID函数

--- funcs/functions.go
+++ funcs/functions.go
@@ -3,12 +3,26 @@ package funcs
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
-// RequestID 生成request_id
+// RequestID 生成request_id; 获取堆栈信息会影响性能，建议debug时用。
 func RequestID() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return strconv.FormatInt(time.Now().UnixNano(), 10) + fmt.Sprintf(":%d", r.Intn(10000))
+}
+
+// GID 获取 goroutine ID
+func GID() int {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+	id, err := strconv.Atoi(idField)
+	if err != nil {
+		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
+	}
+	return id
 }
