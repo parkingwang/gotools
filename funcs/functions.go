68746e5891bafef91c2d7bce1c69a93b70773109
王晓勃 <wxb0328@icloud.com>
2020-01-06 10:15:37 +0800

    docs(funcs): 增加GID函数文档

--- funcs/functions.go
+++ funcs/functions.go
@@ -9,13 +9,13 @@ import (
 	"time"
 )
 
-// RequestID 生成request_id; 获取堆栈信息会影响性能，建议debug时用。
+// RequestID 生成request_id
 func RequestID() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return strconv.FormatInt(time.Now().UnixNano(), 10) + fmt.Sprintf(":%d", r.Intn(10000))
 }
 
-// GID 获取 goroutine ID
+// GID 获取 goroutine ID;获取堆栈信息会影响性能，建议debug时用。
 func GID() int {
 	var buf [64]byte
 	n := runtime.Stack(buf[:], false)
