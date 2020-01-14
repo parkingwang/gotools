c6816c9c0ccbb93c2cd09e7c45ff2190d5ea3649
subing <subing@parkingwang.com>
2020-01-14 09:42:08 +0800

    修改日志输出屏蔽文件

--- gin/middleware/before.go
+++ gin/middleware/before.go
@@ -58,7 +58,7 @@ func logRequest(ctx *gin.Context) {
 	ctx.Request.ParseMultipartForm(1024)
 	logger.Info(RequestID, "******* Client : ", ctx.Request.RemoteAddr, " [", ctx.Request.Method, "] ", ctx.Request.URL.Path, "*******")
 	logger.Info(RequestID, "URL: ", ctx.Request.URL, "ContentType: ", ctx.ContentType())
-	if !strings.Contains(ctx.GetHeader("Content-Disposition"), "name=\"file\"") {
+	if !strings.Contains(ctx.GetHeader("Content-Disposition"), "file") {
 		logger.Info(RequestID, "PostForm:", ctx.Request.PostForm, "JSON:", string(bodyBytes))
 	}
 }
